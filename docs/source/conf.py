2b8706a80e58a2b62d5146d4f3240bd24009c25a
Lionel JOURDHIER <lio.jourdhier@gmail.com>
2026-03-02 12:37:33 +0100

    test d'adressage

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,7 @@ import sys
 
 
 # On remonte à la racine, puis on descend dans 'app'
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('../../app'))
 
 
